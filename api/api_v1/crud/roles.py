f3dcabdeaa565ec54cd7dbeb58f175395efcca86
Dennis Mattews <alttert@mail.ru>
2024-10-21 00:47:50 +0300

    roles crud upd

--- api/api_v1/crud/roles.py
+++ api/api_v1/crud/roles.py
@@ -7,17 +7,21 @@ from api.api_v1.schemas.role import RoleCreate
 from api.core.models.role import Role
 
 
-async def create_role(db: AsyncSession, role: RoleCreate) -> Role:
+async def create_role(
+        session: AsyncSession,
+        role: RoleCreate,
+) -> Role:
     """Создание новой роли."""
-    db_role = Role(name=role.name)
-    db.add(db_role)
-    await db.commit()
-    await db.refresh(db_role)
+    db_role = Role(**role.model_dump())
+    session.add(db_role)
+    await session.commit()
+    await session.refresh(db_role)
     return db_role
 
 
-async def get_all_roles(db: AsyncSession) -> Sequence[Role]:
-    """Получение списка всех ролей."""
+async def get_all_roles(
+        session: AsyncSession,
+) -> Sequence[Role]:
     stmt = select(Role)
-    result = await db.execute(stmt)
+    result = await session.execute(stmt)
     return result.scalars().all()
